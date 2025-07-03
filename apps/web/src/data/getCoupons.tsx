c53f924767a459c64d57f6e5dead56db69b72b63
Mesele Shishay <messeleshishaymm@gmail.com>
2025-07-03 12:02:33 +0300

    refactor: replace dynamic coupon fetching with static coupon data in getCoupons function; remove unused URL import

--- apps/web/src/data/getCoupons.tsx
+++ apps/web/src/data/getCoupons.tsx
@@ -1,10 +1,28 @@
-import {URLS} from '../config';
-
 export async function getCoupons() {
-  const response = await fetch(URLS.GET_COUPONS);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const json = await response.json();
-  return json.coupons;
+  return [
+    {
+      id: 1,
+      discount: 10,
+      activationCode: 'START',
+      title: 'Mobile Application Development',
+    },
+    {
+      id: 2,
+      discount: 30,
+      activationCode: '4386',
+      title: 'UI/UX Design',
+    },
+    {
+      id: 3,
+      discount: 25,
+      activationCode: 'JAVA',
+      title: 'Java from Scratch to Expert',
+    },
+    {
+      id: 4,
+      discount: 40,
+      activationCode: '354715',
+      title: 'Instagram Marketing 2022',
+    },
+  ];
 }
